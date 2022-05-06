d7ddba78f407bee0d78a784819d5493b4ea1d3cf
TeoAle <94782233+TeoAle@users.noreply.github.com>
2022-05-06 23:58:08 +0200

    Update and rename fe_&_model_evaluation.py to feature_engineering.py

---
+++ feature_engineering.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import warnings
+from sklearn.linear_model import LogisticRegression # model we are going to use
+from datetime import datetime
+from Class_ML.ML_Pipeline import MLPipeline # this is the class for the ML Pipeline I created in the folder Class_ML 
+
+warnings.filterwarnings("ignore")
+
+########################################## MODEL EVALUATION ##########################################
+
+
