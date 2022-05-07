3e375accb2cd83fa14980ec7fee374cbbf914bb3
TeoAle <94782233+TeoAle@users.noreply.github.com>
2022-05-07 10:10:02 +0200

    Update model_evaluation.py

--- model_evaluation.py
+++ model_evaluation.py
@@ -12,3 +12,48 @@ warnings.filterwarnings("ignore")
 ########################################## MODEL EVALUATION ##########################################
 
 
+if __name__ == "__main__":
+  
+      # --------------------- Importing the cleaned dataset we created in 'feature_engineering.py' ---------------------
+
+      data_clean = pd.read_csv('data/BTCUSDT_CLEANED.csv')
+      data_clean = data_clean.set_index('timestamp')
+
+      # --------------------- Evaluating the model through our Machine Learning Pipeline Class ---------------------
+
+      # Features we are going to use for prediction
+
+      features = ['excursion',
+                  'BOP',
+                  'momentum',
+                  'typical_price',
+                  'money_flow',
+                  'MACD',
+                  'perc_K',
+                  'perc_D',
+                  'volume',
+                  'number of trades',
+                  'taker buy base asset volume', 
+                  'volatility']
+      
+      target_array = data_clean[['label']].values
+
+      model = LogisticRegression(random_state=42)
+      parameters = {                                       # We give a range of reasonable parameters to be tested through the Grid Search
+                    'C': [0.001,0.01,0.1,1,10,100],        # (in the parameter_tuning function) of the MLPipeline class
+                    'class_weight' : [None, 'Balanced'],
+                    'max_iter' : [100,1000,10000]
+                    }
+
+
+      pipeline = MLPipeline(model=model)
+      pipeline.load_data(dataset=data_clean[features],target=target_array)
+      pipeline.split_data()
+      pipeline.preprocessing()
+      pipeline.parameter_tuning(parameters)
+      pipeline.model_evaluation()
+      pipeline.k_fold_cross_validation()
+      pipeline.report()
+
+  
+  
